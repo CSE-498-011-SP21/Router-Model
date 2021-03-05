def4cb03fbaa1af05ae68159ef2bcaab27ff8ccc
alexschiffman <schiffmanalex@gmail.com>
2021-03-05 09:39:00 -0500

    added example header

---
+++ Model.h
@@ -0,0 +1,34 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+template <typename T>
+class Model
+{
+	private:
+		// Private fields and functions for model
+
+	public:
+		// Public functions for model
+
+		/**
+		 * @brief Empty Model constructor
+		 * @todo Write this functions
+		 */
+		Model()
+		{
+			
+		}
+
+
+		/**
+		 * @brief Classify an input key as hor or cold
+		 * @param key The key to be classified
+		 * @return True if the key is hot and false otherwise
+		 */
+		bool classify(T key)
+		{
+			return false;
+		}
+}
+
+#endif
