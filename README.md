a197850406f6c9f515b5e47a110fbaef45ca4e0a
ads221 <ads221@lehigh.edu>
2021-04-20 10:27:35 -0400

    updated workload gen code and added documentation to README

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # Router-Model
 
-Model use in Router:
-```cpp
-if (model.classify(key))
-{
-    // Send to CPU
-}
-else
-{
-    // Send to GPU
-}
+## Test workload generator (gen directory)
+Install numpy if not installed
+```
+pip install numpy
+```
+
+To run with default arguments, run
+```
+python3 generate.py
+```
+
+Usage with command line arguments
+```
+usage: generate.py [-h] [-l LENGTH] [-s SKEW] [-a ALPHA] [-o OUTFILE]
+-l LENGTH, --length LENGTH  length of the request stream
+-s SKEW, --skew SKEW  ratio of gets in the stream
+-a ALPHA, --alpha ALPHA  Zipfian alpha value (a > 1)
+-o OUTFILE, --outfile OUTFILE  name of the output file ([outfile].csv)
 ```
--- gen/generate.py
+++ gen/generate.py
@@ -7,14 +7,17 @@ parser.add_argument("-l", "--length", type=int, default=100000,
 					help="length of the request stream")
 parser.add_argument("-s", "--skew", type=float, default=0.5,
 					help="ratio of gets in the stream")
+parser.add_argument("-a", "--alpha", type=float, default=1.5,
+					help="Zipfian alpha value (a > 1)")
 parser.add_argument("-o", "--outfile", type=str,
 					default="workload",
-					help="name of the workload save file")
+					help="name of the output file ([outfile].csv)")
 args = parser.parse_args()
 
 # check if provided args are correct
 assert args.length >= 0
 assert args.skew >= 0 and args.skew <= 1
+assert args.alpha > 1
 
 # assign global variables
 MAXINT = 2**32 - 1
@@ -27,7 +30,7 @@ d = decimal.Decimal(str(GETS)).as_tuple()
 mul = 10**(-d.exponent) * (1 + (d.digits[-1] % 2 != 0))
 
 # generate a Zipfian distribution of keys with a given length
-x = np.random.zipf(1.5, LENGTH)
+x = np.random.zipf(args.alpha, LENGTH)
 
 # loop through the keys and assign request data
 out = []
@@ -48,6 +51,12 @@ for i in x:
 	
 	# append the request
 	out.append([i, request_type, value])
+
+# print out stats
+print("Generated test workload with:")
+print(f"{LENGTH} requests")
+print(f"{len(set(x))} unique keys")
+print(f"Saved to {args.outfile}.csv")
 
 # write data to CSV file
 with open(args.outfile + '.csv', mode='w') as csv_file:
